81b099fb96738457e0d165db184bc742e1dfa8e9
ogola <ogolajeremy601@gmail.com>
2024-04-13 14:06:30 +0300

    adds-simple-liker

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = {
+  "♡": "♥",
+  "♥": "♡"
+};
+const FULL_HEART = {
+  "red" : "",
+  "": "red"
+};
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph")
+
+function like(e){
+  const heart = e.target;
+  mimicServerCall()
+    .then(function(serverResponse){
+      alert("server notified");
+      alert(serverResponse);
+      heart.innerText = EMPTY_HEART[heart.innerText];
+      heart.style.color = FULL_HEART[heart.style.color];
+    })
+    .catch(function(error){
+      alert("Something went wrong!");
+    })
+}
+
+for(const glyph of articleHearts){
+  glyph.addEventListener("click", like)
+}
 
 
 
